66bc8a2324bed3a7685d87d989f230dfea6ee4fc
Maurits <maurits.smedberg@elev.ga.ntig.se>
2023-03-29 12:48:05 +0200

    First push

---
+++ projekt_api.py
@@ -0,0 +1,17 @@
+import requests
+response = requests.get("https://randomuser.me/api/")
+
+def newLines(number):
+    for i in range(number):
+        print('')
+
+
+print(type(response))
+status_code = response.status_code 
+print("Status code is: ", status_code,newLines(10))
+
+
+#Plocka ut relevanta delar ur requests body för att manipulera på något sätt
+#Skapa relevanta klasser och arv som kan uppta den omvandlade informationen
+#Test cases; Försök få full code coverage(testen går igenom alla kodrader)
+#Skapa någon typ av GUI(Valbart)
